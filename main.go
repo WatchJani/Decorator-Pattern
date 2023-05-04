c434042536cf869610dc93253076e705d8473ea5
WatchJani <jankokondic84@gmail.com>
2023-05-04 18:37:18 +0200

    simple example

--- main.go
+++ main.go
@@ -2,7 +2,44 @@ package main
 
 import "fmt"
 
+type optFunc func(*Setting)
+
+type Setting struct {
+	Moves int
+	Time  string
+}
+
+func SetMoves(moves int) optFunc {
+	return func(opts *Setting) {
+		opts.Moves = moves
+	}
+}
+
+func Default() Setting {
+	return Setting{
+		Moves: 0,
+		Time:  "00:00",
+	}
+}
+
+type Games struct {
+	Setting
+}
+
+func New(opts ...optFunc) *Games {
+	S := Default()
+
+	for _, fn := range opts {
+		fn(&S)
+	}
+
+	return &Games{
+		Setting: S,
+	}
+}
+
 func main() {
-	fmt.Println("asd")
-	fmt.Println("asd")
+	myGame := New(SetMoves(2000))
+
+	fmt.Println(myGame)
 }
